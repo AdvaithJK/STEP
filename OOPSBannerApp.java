711721047c14339466339df3d2eb270e2cc5a988
AdvaithJK <aj2099@srmist.edu.in>
2026-03-03 08:56:51 +0530

    UC2 - Print OOPS Banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,7 +1,21 @@
+/**
+ * OOPSBannerApp UC2 – Banner Display Application
+ *
+ * Prints OOPS in banner format using * and spaces.
+ */
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("  *****   " + "  *****   " + " *******  " + "  *****  ");
+        System.out.println(" *     *  " + " *     *  " + " *      * " + " *     * ");
+        System.out.println("*       * " + "*       * " + " *      * " + " *       ");
+        System.out.println("*       * " + "*       * " + " ******   " + "  *****  ");
+        System.out.println("*       * " + "*       * " + " *        " + "       * ");
+        System.out.println(" *     *  " + " *     *  " + " *        " + " *     * ");
+        System.out.println("  *****   " + "  *****   " + " *        " + "  *****  ");
+
     }
 
 }
